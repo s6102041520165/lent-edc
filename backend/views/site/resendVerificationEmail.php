afabd39349e75fa27312942b737c41d81ec41c22
Petch <s6102041520165@email.kmutnb.ac.th>
2020-02-18 20:38:42 +0700

    changed

--- backend/views/site/resendVerificationEmail.php
+++ backend/views/site/resendVerificationEmail.php
@@ -12,7 +12,7 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="site-resend-verification-email">
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>Please fill out your email. A verification email will be sent there.</p>
+    <p>กรุณาป้อนอีเมล. ระบบจะส่งลิงค์ยืนยันตัวตนให้ท่าน.</p>
 
     <div class="row">
         <div class="col-lg-5">
--- backend/views/site/resetPassword.php
+++ backend/views/site/resetPassword.php
@@ -11,9 +11,8 @@ $this->title = 'Reset password';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-reset-password">
-    <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>Please choose your new password:</p>
+    <p>กรุณาป้อนอีเมล:</p>
 
     <div class="row">
         <div class="col-lg-5">
