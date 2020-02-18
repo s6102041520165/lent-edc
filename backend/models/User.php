f98d1d0c440fa82783da40e6f1e3c677fc6113c0
Petch <s6102041520165@email.kmutnb.ac.th>
2020-02-18 14:16:41 +0700

    added

--- backend/models/User.php
+++ backend/models/User.php
@@ -2,8 +2,7 @@
 
 namespace app\models;
 
-use Yii;
-use yii\base\Model;
+use common\models\User as ModelsUser;
 
 /**
  * This is the model class for table "user".
@@ -19,27 +18,7 @@ use yii\base\Model;
  * @property int $updated_at
  * @property string|null $verification_token
  */
-class User extends Model
+class User extends ModelsUser
 {
-    public $username;
-    public $new_password;
-    public $retry_password;
-    public $old_password;
-
-    public function rules()
-    {
-        return [
-            [['username','new_password','retry_password','old_password'],'string'],
-        ];
-    }
-
-    public function attributeLabels()
-    {
-        return [
-            'username' => 'ชื่อผู้ใช้',
-            'old_password'=> 'รหัสผ่านเก่า',
-            'retry_password' => 'ยืนยันรหัสผ่าน',
-            'new_password' => 'รหัสผ่านใหม่',
-        ];
-    }
+    
 }
---
+++ backend/models/UserProfile.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+use yii\base\Model;
+
+/**
+ * This is the model class for table "user".
+ *
+ * @property int $id
+ * @property string $username
+ * @property string $auth_key
+ * @property string $password_hash
+ * @property string|null $password_reset_token
+ * @property string $email
+ * @property int $status
+ * @property int $created_at
+ * @property int $updated_at
+ * @property string|null $verification_token
+ */
+class User extends Model
+{
+    public $username;
+    public $new_password;
+    public $retry_password;
+    public $old_password;
+
+    public function rules()
+    {
+        return [
+            [['username','new_password','retry_password','old_password'],'string'],
+        ];
+    }
+
+    public function attributeLabels()
+    {
+        return [
+            'username' => 'ชื่อผู้ใช้',
+            'old_password'=> 'รหัสผ่านเก่า',
+            'retry_password' => 'ยืนยันรหัสผ่าน',
+            'new_password' => 'รหัสผ่านใหม่',
+        ];
+    }
+}
