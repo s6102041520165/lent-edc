2a0dbd40a225cfd738a5c693d3dc3c3ea03d7602
Petch <s6102041520165@email.kmutnb.ac.th>
2020-02-19 13:06:26 +0700

    updated

--- backend/views/edc/_print.php
+++ backend/views/edc/_print.php
@@ -4,14 +4,15 @@ use yii\helpers\Html;
 use yii\widgets\ListView;
 
 ?>
-<?php
-echo ListView::widget([
-    'dataProvider' => $model,
-    'itemView' => '_data',
-    'viewParams' => [
-        'fullView' => true,
-        'context' => 'main-page',
-    ],
-]);
-?>
-<barcode code="12345678" type="c93" size="0.8" height="2.0" />
+<p>
+<barcode code="TX1245584465" type="c93" />
+</p>
+<p>
+<barcode code="TX5458115457" type="c93" />
+</p>
+<p>
+<barcode code="TX2244125741" type="c93" />
+</p>
+<p>
+<barcode code="TX3025485545" type="c93" />
+</p>
