b208af643c9fa5fd4812bddbbe5a608b2d6e198d
Petch <s6102041520165@email.kmutnb.ac.th>
2020-02-27 10:24:03 +0700

    updated by petch

--- backend/views/lent/update.php
+++ backend/views/lent/update.php
@@ -5,7 +5,7 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model app\models\Lent */
 
-$this->title = Yii::$app->formatter->format($model->created_at, 'datetime');
+$this->title = Yii::$app->formatter->format($model->updated_at, 'datetime');
 $this->params['breadcrumbs'][] = ['label' => 'ระบบเบิกจ่ายเครื่อง EDC', 'url' => ['index']];
 $this->params['breadcrumbs'][] = ['label' => $model->id, 'url' => ['view', 'id' => $model->created_at]];
 $this->params['breadcrumbs'][] = 'แก้ไข';
